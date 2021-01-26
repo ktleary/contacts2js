aeee2f819d8ec369fa720730219f0923031ba09b
ktleary <kevin@ktleary.com>
2021-01-27 07:20:30 +0800

    add tests, change FamilyName to familyName in Contact

---
+++ test/contacts2csv.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+const test = require("ava");
+const fs = require("fs");
+const path = require("path");
+const contacts2js = require("../index");
+const contactspath = "./test/test-contacts.csv";
+
+const getFile = (filepath) => fs.readFileSync(filepath, { encoding: "utf-8" });
+
+test("it should return an object", (t) => {
+  const contacts = getFile(contactspath);
+  const jsContacts = contacts2js(contacts);
+  t.is(Array.isArray(jsContacts), true);
+});
+
+test("it should find Aaron Jones", (t) => {
+  const contacts = getFile(contactspath);
+  const jsContacts = contacts2js(contacts);
+
+  const jones = jsContacts.find((contact) => contact.familyName === "Jones");
+  console.log(jones.size);
+  t.is(jones.familyName, "Jones");
+});
+
+test("it should be true for hello", (t) => {
+  const hello = "hello";
+  t.assert(hello, "hello");
+});
