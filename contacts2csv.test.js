137c68ad498f73837372a46da642a62fb91ab96d
ktleary <kevin@ktleary.com>
2021-01-26 11:38:38 +0800

    create new test -- todo, create mock data

--- contacts2csv.test.js
+++ contacts2csv.test.js
@@ -1,5 +1,13 @@
 /* eslint-disable */
 const test = require("ava");
+const contacts2js = require("./index");
+const contact = require("./docs/example-google-contact");
+
+// failing test -- need a sample csv
+test("it should return an object", (t) => {
+  const jsContact = contacts2js(contact);
+  t.assert(typeof jsContact, object);
+});
 
 test("it should be true for hello", (t) => {
   const hello = "hello";
