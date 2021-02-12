4cb01c395840bbb3821c751baf9dc9a056ef249e
ktleary <kevin@ktleary.com>
2021-02-12 19:59:08 +0800

    update csv, eslint, remove comments

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const parse = require("csv-parse/lib/sync");
 const { HEADINGS, createContact } = require("./lib/Contact");
 
-function getRecords(csvdata) {
-  if (!csvdata) return [];
-  return parse(csvdata, {
-    columns: true,
-    skip_empty_lines: true,
-  });
-}
+const getRecords = (csvdata) =>
+  csvdata
+    ? parse(csvdata, {
+        columns: true,
+        skip_empty_lines: true,
+      })
+    : [];
 
-// contacts2js :: string -> [Contact]
-function contacts2js(csvContacts = "") {
-  const records = getRecords(csvContacts);
-  return records.map((record) => createContact(record, HEADINGS));
-}
+const contacts2js = (csvContacts) =>
+  getRecords(csvContacts).map((record) => createContact(record, HEADINGS));
 
 /* eslint-disable fp/no-mutation */
 module.exports = contacts2js;
