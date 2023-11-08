ddfc0ee385893677b030a6f3f9a846df7d5c60a1
djlooop <djlooop@stringtalk.org>
2023-11-07 23:32:46 -0500

    bump deps

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const parse = require("csv-parse/lib/sync");
 const { HEADINGS, createContact } = require("./lib/Contact");
 
-const getRecords = (csvdata) =>
-  csvdata
-    ? parse(csvdata, {
-        columns: true,
-        skip_empty_lines: true,
-      })
-    : [];
+const getRecords = (csvdata) => {
+  try {
+    return csvdata
+      ? parse(csvdata, {
+          columns: true,
+          skip_empty_lines: true,
+        })
+      : [];
+  } catch (err) {
+    console.error(`Failed to parse CSV data: ${err}`);
+    return [];
+  }
+};
 
 const contacts2js = (csvContacts) =>
   getRecords(csvContacts).map((record) => createContact(record, HEADINGS));
